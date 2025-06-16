0a0104a4a2a6d07248b8e85a673b4454ff85a7ef
jabezinu <jabezzinu31@gmail.com>
2025-06-17 00:30:49 +0300

    another update on server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use('/books', router)
 
 mongoose
